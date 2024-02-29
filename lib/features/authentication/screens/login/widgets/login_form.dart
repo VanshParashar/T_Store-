facc090327ba21e3e2a530d2bd5b73e1049b67a8
primeeeeaa <vanshparashar31@gmail.com>
2024-03-01 00:45:28 +0530

    nvigation bar

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:t_store/features/authentication/screens/signup/signup.dart';
+import 'package:t_store/navigation_menu.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 
@@ -62,7 +63,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                    onPressed: () {}, child: Text(TTexts.signIn))),
+                    onPressed: ()=> Get.to(()=>const NavigationMenu()), child: Text(TTexts.signIn))),
             const SizedBox(
               height: TSizes.spaceBtwItems,
             ),
@@ -72,7 +73,7 @@ class TLoginForm extends StatelessWidget {
                 width: double.infinity,
                 child: OutlinedButton(
                     onPressed: () {
-                      Get.to(()=>SignupScreen());
+                      Get.to(()=>const SignupScreen());
                     }, child: const Text(TTexts.createAccount))),
           ],
         ),
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/shop/screens/home/home.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      bottomNavigationBar: Obx(()=> NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedindex.value,
+          onDestinationSelected: (index){
+            controller.selectedindex.value = index;
+          },
+        backgroundColor: darkMode?TColors.black:TColors.white,
+        indicatorColor: darkMode?TColors.white.withOpacity(0.1): TColors.black.withOpacity(0.1),
+          destinations: const[
+           NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+           NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+           NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+           NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+
+          ],
+        ),
+      ),
+      body: Obx(()=> controller.screens[controller.selectedindex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController{
+  final RxInt selectedindex =0.obs;
+
+  final screens =[HomeScreen(),Container(color: Colors.red,),Container(color: Colors.orangeAccent,),Container(color: Colors.blue,),];
+
+}
